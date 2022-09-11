48efb114e0e7200d3e8f2c972a9be5577c5fee6d
hanmanthudekar <hanmanthudekar@gmail.com>
2022-09-11 17:49:35 +0530

    First commit

---
+++ src/bankingProject/Setup.java
@@ -0,0 +1,18 @@
+package bankingProject;
+
+public class Setup {
+public static void main(String[] args) {
+	boolean t=507494949>467478478;
+System.out.println(t);
+	System.out.println("PHONEPE");
+	p();
+	q();
+}
+public static void p() {
+	System.out.println("Hii Friends");
+	
+}
+public static void q() {
+	System.out.println("We are starting new project");
+}
+}
